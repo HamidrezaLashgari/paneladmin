c85d262543d2368241d720da7b3718c41a786697
Hamidreza <hamidlashgari1998@gmail.com>
2022-01-25 22:59:27 +0330

    Add ant table

--- src/components/person/List.js
+++ src/components/person/List.js
@@ -1,24 +1,41 @@
+import { Table } from "antd";
 import axios from "axios";
 import React, { Component } from "react";
 
 export default class List extends Component {
+  state = {
+    persons: [],
+    loading: true,
+  };
 
+  columns = [
+    { title: "شناسه", key: "id", dataIndex: "id" },
+    { title: "نام", key: "name", dataIndex: "name" },
+    {
+      title: "آدرس",
+      key: "address",
+      dataIndex: "address",
+      render: (field, record, index) => {
+        return `${field.city}, ${field.street} | Phone:${record.phone}`;
+      },
+    },
+  ];
 
-    state = {
-        persons: []
-    }
-
-
-    componentDidMount () {
-        axios('https://jsonplaceholder.typicode.com/users')
-        .then(({data}) => this.setState({persons: data}))
-    }
-
+  componentDidMount() {
+    axios("https://jsonplaceholder.typicode.com/users")
+    .then(({data}) => this.setState({persons: data}))
+    .finally(()=> this.setState({loading: false}))
+  }
 
   render() {
     return (
       <div>
-          persons
+        <Table
+          rowKey={"id"}
+          dataSource={this.state.persons}
+          columns={this.columns}
+          loading={this.state.loading}
+        />
       </div>
     );
   }
