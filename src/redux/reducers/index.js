ac12b3c29c9045a74131758aaac1ff8d6edfaa01
Hamidreza <hamidlashgari1998@gmail.com>
2022-01-30 18:38:25 +0330

    Split reducers to 2part and use combineReducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,12 +1,7 @@
-export default function reducer(state = { persons: [], posts: [] }, action) {
-  switch (action.type) {
-    case 'PERSONS':
-      return { ...state, persons: action.payload }
-
-    case 'POSTS':
-      return { ...state, posts: action.payload }
-
-    default:
-      return state
-  }
-}
+import { combineReducers } from 'redux'
+import { persons } from './person'
+import { posts } from './post'
+export default combineReducers({
+  persons,
+  posts,
+})
